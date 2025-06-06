cc0aadd155cae6c028948550e1695fc6a82e72b2
olgKuz <kuznecovao@list.ru>
2025-06-06 18:21:20 +0300

    4th commit

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const http = require('http');
-const hostname = '127.0.0.1'; const port = 3002;
+const hostname = '127.0.0.1'; const port = 3030;
 const server = http.createServer((req, res) => { res.statusCode = 200;
 res.setHeader('Content-Type', 'text/plain'); res.end('Hello World');
 });
